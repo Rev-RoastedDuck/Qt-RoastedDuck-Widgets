59d5e27647a0168812cacad2d137b2dbe5a9d17a
Rev_RoastedDuck <111643395+Rev-RoastedDuck@users.noreply.github.com>
2023-09-06 18:23:02 +0800

    Add files via upload

---
+++ ShimmerButton/demo.py
@@ -0,0 +1,40 @@
+from PySide6.QtCore import QRect
+from PySide6.QtGui import QColor, QFont
+from PySide6.QtWidgets import QApplication, QWidget
+
+from ShimmerButton import ShimmerButton
+
+if __name__ == '__main__':
+    app = QApplication()
+    w = QWidget()
+    w.resize(1920, 1080)
+    w.move(0, 0)
+    w.setStyleSheet("background-color:#000000")
+
+    # shimmer_color_1: 渐变颜色1
+    # shimmer_color_2: 渐变颜色2
+    # shimmer_blur_radius: 微光扩散程度
+    # timer_interval: 微光流动时间间隔，数值越小，流动速度越快，默认值为4
+    shimmer_button = ShimmerButton(w)
+    shimmer_button.setParams(shimmer_color_1=QColor("#f9ea49"),
+                             shimmer_color_2=QColor("#8bd59a"),
+                             shimmer_blur_radius=60,
+                             timer_interval=2
+                             )
+    shimmer_button.setGeometry(QRect(550, 200, 500, 250))
+    shimmer_button.setStyleSheet("""ShimmerButton{
+                                        background-color: rgba(255, 255, 0,0);
+                                        color:#ffffff;
+                                        border-radius:90px;
+                                        }
+                                """
+                                 )
+
+    # 设置文字和字体
+    font = QFont()
+    font.setPointSize(40)
+    shimmer_button.setFont(font)
+    shimmer_button.setText("BUTTON")
+
+    w.show()
+    app.exec()
