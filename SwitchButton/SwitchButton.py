d67c9adc8b0b9af45745fabbdcfdd15a5e041dc6
Rev_RoastedDuck <111643395+Rev-RoastedDuck@users.noreply.github.com>
2023-10-11 23:05:26 +0800

    Add files via upload

---
+++ SwitchButton/SwitchButton.py
@@ -0,0 +1,335 @@
+from PySide6.QtGui import QColor, QPainter
+from PySide6.QtWidgets import QApplication, QToolButton, QWidget
+from PySide6.QtCore import Qt, Property, QPoint, QPropertyAnimation, QRect
+
+class SwitchButtonCommon(QToolButton):
+    def __init__(self, parent: QWidget = None):
+        super().__init__(parent)
+        self.setCheckable(True)
+        self._indicatorX = 0
+
+        self.animaInit()
+
+    def animaInit(self):
+        self.slideAni = QPropertyAnimation(self, b'indicatorX')
+        self.slideAni.setDuration(200)
+
+    def animaParamsInit(self):
+        self.start = self.height() // 2
+        self.end = self.width() - 2 - self.start
+        self._indicatorX = self.start
+
+    def setParams(self):
+        pass
+
+    def toggle(self):
+        self.setChecked(not self.isChecked())
+
+    def mouseReleaseEvent(self, event) -> None:
+        super().mouseReleaseEvent(event)
+        if self.isChecked():
+            self.slideAni.setEndValue(self.end)
+        else:
+            self.slideAni.setEndValue(self.start)
+        self.slideAni.start()
+
+    def paintEvent(self, event):
+        painter = QPainter(self)
+        painter.setRenderHints(QPainter.Antialiasing)
+        self.paintBackground(painter)
+        self.paintIndicator(painter)
+
+    def paintBackground(self, painter: QPainter):
+        pass
+
+    def paintIndicator(self, painter: QPainter):
+        pass
+
+    @Property(float)
+    def indicatorX(self):
+        return self._indicatorX
+
+    @indicatorX.setter
+    def indicatorX(self, x):
+        self._indicatorX = x
+        self.update()
+
+    def showEvent(self, event):
+        super().showEvent(event)
+        self.animaParamsInit()
+
+
+class SwitchButton(SwitchButtonCommon):
+    def setParams(self,
+                  indicator_color: QColor = QColor(255, 255, 255),
+                  background_color: QColor = QColor(188, 188, 188),
+                  checked_background_color: QColor = QColor(0, 89, 89),
+                  checked_indicator_color: QColor = None
+                  ):
+        self.indicator_color = indicator_color
+        self.background_color = background_color
+        self.checked_background_color = checked_background_color
+
+    def paintBackground(self, painter: QPainter):
+        painter.save()
+        r = self.height() // 2
+        rect = QRect(1, 1, self.width() - 2, self.height() - 1)
+
+        if not self.isChecked():
+            painter.setBrush(self.background_color)
+        else:
+            painter.setBrush(self.checked_background_color)
+
+        painter.setPen(Qt.NoPen)
+        painter.drawRoundedRect(rect, r, r)
+        painter.restore()
+
+    def paintIndicator(self, painter: QPainter):
+        painter.save()
+        r = self.height() // 3
+        point = QPoint(int(self._indicatorX), 0)
+
+        painter.translate(0, self.height() // 2)
+        painter.setPen(Qt.NoPen)
+        painter.setBrush(self.indicator_color)
+        painter.drawEllipse(point, r, r)
+        painter.restore()
+
+
+class SwitchButton_2(SwitchButtonCommon):
+    def setParams(self,
+                  indicator_color: QColor = QColor(181, 181, 181),
+                  background_color: QColor = QColor(181, 181, 181),
+                  checked_background_color: QColor = QColor(255, 167, 38),
+                  checked_indicator_color: QColor = QColor(255, 167, 38)
+                  ):
+        self.indicator_color = indicator_color
+        self.background_color = background_color
+        self.checked_background_color = checked_background_color
+        self.checked_indicator_color = checked_indicator_color
+
+    def paintBackground(self, painter: QPainter):
+        painter.save()
+        r = self.height() // 2
+        rect = QRect(1, 1, self.width() - 2, self.height() - 2)
+
+        if self.isChecked():
+            painter.setPen(self.checked_background_color)
+        else:
+            painter.setPen(self.background_color)
+
+        painter.drawRoundedRect(rect, r, r)
+        painter.restore()
+
+    def paintIndicator(self, painter: QPainter):
+        painter.save()
+        r = self.height() // 3
+        point = QPoint(int(self._indicatorX), 0)
+
+        painter.translate(0, self.height() // 2)
+
+        if self.isChecked():
+            painter.setBrush(self.checked_indicator_color)
+        else:
+            painter.setBrush(self.indicator_color)
+        painter.setPen(Qt.NoPen)
+        painter.drawEllipse(point, r, r)
+        painter.restore()
+
+
+class SwitchButton_3(SwitchButtonCommon):
+    def setParams(self,
+                  indicator_color: QColor = QColor(255, 255, 255),
+                  background_color: QColor = QColor(204, 204, 204),
+                  checked_background_color: QColor = QColor(51, 102, 153, 200),
+                  checked_indicator_color: QColor = QColor(51, 102, 153)
+                  ):
+        self.indicator_color = indicator_color
+        self.background_color = background_color
+        self.checked_background_color = checked_background_color
+        self.checked_indicator_color = checked_indicator_color
+
+    def paintBackground(self, painter: QPainter):
+        painter.save()
+
+        w = self.width() - self.width() // 3
+        h = self.height() - self.height() // 3
+        rect = QRect(-w // 2, -h // 2, w, h)
+        r = h // 2
+
+        painter.translate(self.width() // 2, self.height() // 2)
+
+        if self.isChecked():
+            painter.setBrush(self.checked_background_color)
+        else:
+            painter.setBrush(self.background_color)
+        painter.setPen(Qt.NoPen)
+        painter.drawRoundedRect(rect, r, r)
+        painter.restore()
+
+    def paintIndicator(self, painter: QPainter):
+        painter.save()
+        r = (self.height() - self.height() // 3) // 2 + self.height() // 9
+        point = QPoint(int(self._indicatorX), 0)
+
+        painter.translate(0, self.height() // 2)
+
+        if self.isChecked():
+            painter.setBrush(self.checked_indicator_color)
+        else:
+            painter.setBrush(self.indicator_color)
+        painter.setPen(Qt.NoPen)
+        painter.drawEllipse(point, r, r)
+        painter.restore()
+
+
+class SwitchButton_4(SwitchButtonCommon):
+    def animaParamsInit(self):
+        super().animaParamsInit()
+        self.isHover = False
+
+    def setParams(self,
+                  indicator_color: QColor = QColor(255, 255, 255),
+                  background_color: QColor = QColor(204, 204, 204),
+                  checked_background_color: QColor = QColor(153, 0, 51, 200),
+                  checked_indicator_color: QColor = QColor(153, 0, 51)
+                  ):
+        self.indicator_color = indicator_color
+        self.background_color = background_color
+        self.checked_background_color = checked_background_color
+        self.checked_indicator_color = checked_indicator_color
+
+    def enterEvent(self, event):
+        super().enterEvent(event)
+        self.isHover = True
+
+    def leaveEvent(self, event):
+        super().leaveEvent(event)
+        self.isHover = False
+
+    def paintBackground(self, painter: QPainter):
+        painter.save()
+
+        w = self.width() - self.width() // 2
+        h = self.height() - self.height() // 2
+
+        r = h // 2
+
+        painter.translate(self.width() // 2, self.height() // 2)
+
+        rect = QRect(-w // 2, -h // 2, w, h)
+
+        painter.setPen(Qt.NoPen)
+        if self.isChecked():
+            painter.setBrush(self.checked_background_color)
+        else:
+            painter.setBrush(self.background_color)
+
+        painter.drawRoundedRect(rect, r, r)
+        painter.restore()
+
+    def paintIndicator(self, painter: QPainter):
+        painter.save()
+        r = (self.height() - self.height() // 2) // 2 + self.height() // 9
+        point = QPoint(int(self._indicatorX), 0)
+
+        painter.translate(0, self.height() // 2)
+        painter.setPen(Qt.NoPen)
+        if self.isChecked():
+            if self.isHover:
+                c = self.checked_indicator_color
+                painter.setBrush(QColor(c.red(), c.green(), c.blue(), 100))
+                painter.drawEllipse(point, r + 5, r + 5)
+            painter.setBrush(self.checked_indicator_color)
+
+        else:
+            if self.isHover:
+                c = self.indicator_color
+                painter.setBrush(QColor(c.red(), c.green(), c.blue(), 100))
+                painter.drawEllipse(point, r + 5, r + 5)
+            painter.setBrush(self.indicator_color)
+
+        painter.drawEllipse(point, r, r)
+
+        painter.restore()
+
+
+class SwitchButton_5(SwitchButtonCommon):
+    def setParams(self,
+                  indicator_color: QColor = QColor(255, 255, 255),
+                  background_color: QColor = QColor(204, 204, 204),
+                  checked_background_color: QColor = QColor(153, 0, 51, 200),
+                  ):
+        self.indicator_color = indicator_color
+        self.background_color = background_color
+        self.checked_background_color = checked_background_color
+
+    def animaParamsInit(self):
+        self.var = self.height() // 8
+        h = self.height() - self.var * 2
+        self.start = self.var
+        self.end = self.width() - h - self.var
+        self._indicatorX = self.var
+
+    def paintBackground(self, painter: QPainter):
+        painter.save()
+        if self.isChecked():
+            painter.setBrush(self.checked_background_color)
+        else:
+            painter.setBrush(self.background_color)
+
+        painter.setPen(Qt.NoPen)
+        painter.drawRoundedRect(self.rect(), 5, 5)
+        painter.restore()
+
+    def paintIndicator(self, painter: QPainter):
+        painter.save()
+        h = self.height() - self.var * 2
+        rect = QRect(self._indicatorX, -h // 2, h, h)
+
+        painter.translate(0, self.height() // 2)
+        painter.setPen(Qt.NoPen)
+        painter.setBrush(self.indicator_color)
+
+        painter.drawRoundedRect(rect, 5, 5)
+        painter.restore()
+
+
+class Window(QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent=parent)
+        self.resize(1200, 800)
+        self.move(0, 0)
+
+        self.switchButton_1 = SwitchButton(parent=self)
+        self.switchButton_1.setGeometry(400, 200, 80, 40)
+        self.switchButton_1.setParams(checked_background_color=QColor(0, 89, 89))
+
+        self.switchButton_2 = SwitchButton_2(parent=self)
+        self.switchButton_2.setGeometry(500, 200, 80, 40)
+        self.switchButton_2.setParams(checked_background_color=QColor(0, 89, 89),
+                                      checked_indicator_color=QColor(0, 89, 89)
+                                      )
+
+        self.switchButton_3 = SwitchButton_3(parent=self)
+        self.switchButton_3.setGeometry(385, 250, 100, 50)
+        self.switchButton_3.setParams(checked_background_color=QColor(0, 89, 89, 200),
+                                      checked_indicator_color=QColor(255, 255, 255))
+
+        self.switchButton_4 = SwitchButton_4(parent=self)
+        self.switchButton_4.setGeometry(480, 250, 100, 50)
+        self.switchButton_4.setParams(checked_background_color=QColor(0, 89, 89, 200),
+                                      checked_indicator_color=QColor(255, 255, 255))
+
+        self.switchButton_5 = SwitchButton_5(parent=self)
+        self.switchButton_5.setGeometry(400, 310, 70, 35)
+        self.switchButton_5.setParams(background_color=QColor(188, 188, 188),
+                                      checked_background_color=QColor(0, 89, 89),
+                                      indicator_color=QColor(255, 255, 255))
+
+
+if __name__ == '__main__':
+    app = QApplication()
+    w = Window()
+    w.show()
+    app.exec()
