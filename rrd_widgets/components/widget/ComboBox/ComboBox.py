1a41d7f590752579a4d727d53e0fb8b3e80275fa
Rev_RoastedDuck <111643395+Rev-RoastedDuck@users.noreply.github.com>
2024-02-04 21:48:54 +0800

    Update ComboBox.py

--- rrd_widgets/components/widget/ComboBox/ComboBox.py
+++ rrd_widgets/components/widget/ComboBox/ComboBox.py
@@ -1,13 +1,30 @@
+"""
+Qt-RoastedDuck-Widgets
+======================
+Qt widgets-based implementation of the Material Design specification.
+
+Repository at https://github.com/Rev-RoastedDuck/Qt-RoastedDuck-Widgets.
+
+Demo are available at https://github.com/Rev-RoastedDuck/Qt-RoastedDuck-Widgets/tree/main/Demo.
+
+Examples are available at https://github.com/Rev-RoastedDuck/Qt-RoastedDuck-Widgets/tree/main/examples.
+
+Information:
+    WeChat: Roast_71.
+    csdnBlog: https://blog.csdn.net/m0_72760466?type=blog.
+
+:copyright: (c) 2023 by Rev-RoastedDuck.
+:license: GPLv3, see LICENSE for more details.
+"""
+
 from typing import TypedDict, Any, List
 
-from PySide6.QtCore import QRect, QPropertyAnimation, QSize, QTimer, Qt, Signal, QPoint, QEvent, QEasingCurve, Property
-from PySide6.QtGui import QShowEvent, QIcon, QColor, QPainterPath, QPainter, QBrush, QPen, QTextOption, QEnterEvent, \
-    QMouseEvent, QFont
 from PySide6.QtWidgets import QWidget, QGraphicsDropShadowEffect, QPushButton, QHBoxLayout
+from PySide6.QtCore import QRect, QPropertyAnimation, QSize, QTimer, Qt, Signal, QPoint, QEvent, QEasingCurve
+from PySide6.QtGui import QShowEvent, QIcon, QColor, QPainterPath, QPainter, QBrush, QPen, QTextOption, QEnterEvent,QMouseEvent, QFont
 
-from ..LineEdit.SimpleLineEdit import SimpleLineEdit_1
 from ...layout.RVBoxLayout import VBoxLayoutManager
-from ....common.icon.raw_icon import icon as icon
+from ....common.icon.binary_data_icon import icon_comboBox as icon
 
 
 class ComboBoxItem(TypedDict):
